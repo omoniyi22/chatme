e87e0ec5fb1a3cc73be72cc4a086b7a51d51c48f
omoniyi22 <omoniyioluwaseun22gmail.com>
2023-02-20 23:50:59 +0100

    chat_app

---
+++ build/precache-manifest.86523ca8f81da69cc66f1414940956eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81b2d8b2822ebafa8fbd47ce75a5b68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "133e1e221e9e47b1e5f6",
+    "url": "/static/css/main.7bd0622a.chunk.css"
+  },
+  {
+    "revision": "9d56b53cf7a6555c96b8",
+    "url": "/static/js/2.2a7c913e.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.2a7c913e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133e1e221e9e47b1e5f6",
+    "url": "/static/js/main.4d245b73.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
